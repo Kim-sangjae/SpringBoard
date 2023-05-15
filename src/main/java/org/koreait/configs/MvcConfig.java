74bcaf992ab519c53ab413aa3547a719adc4db98
Kim-sangjae <kimsangjae94114@gmail.com>
2023-05-15 12:51:13 +0900

    template 레이아웃 세팅

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -1,18 +1,27 @@
 package org.koreait.configs;
 
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
 
     //파일업로드 부분 설정 properties에 설정한 경로를 지정해준다
     @Value("${file.upload.path}")
     private String fileUploadPath;
+
+
+    // 사이트 설정 유지 인터셉터 (인터셉터 클래스에 @component 붙였는지 확인)
+    private final SiteConfigInterceptor siteConfigInterceptor;
+
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -27,6 +36,13 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:///"+fileUploadPath);
     }
 
+
+    //인터셉터
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(siteConfigInterceptor)
+               .addPathPatterns("/**");
+    }
 
 
 }
---
+++ src/main/java/org/koreait/configs/interceptors/SiteConfigInterceptor.java
@@ -0,0 +1,25 @@
+package org.koreait.configs.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/*
+* 사이트 설정 유지
+* 사이트 전역에 공통부분으로 쓰이는 것들 설정
+* */
+@Component
+public class SiteConfigInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        request.setAttribute("cssJsVersion",1);
+
+        return true;
+    }
+
+
+}
