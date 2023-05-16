300d2b5db9fe5003ec2590b4ae77e9c81a910120
Kim-sangjae <kimsangjae94114@gmail.com>
2023-05-16 09:43:45 +0900

    회원엔티티작성

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableJpaAuditing
 public class MvcConfig implements WebMvcConfigurer {
 
     //파일업로드 부분 설정 properties에 설정한 경로를 지정해준다
