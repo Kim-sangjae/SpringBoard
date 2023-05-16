a9f8a8421040041509677bae4df443f6514442e9
Kim-sangjae <kimsangjae94114@gmail.com>
2023-05-16 09:55:43 +0900

    멤버 dto 생성

---
+++ src/main/java/org/koreait/controllers/members/JoinForm.java
@@ -0,0 +1,23 @@
+package org.koreait.controllers.members;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinForm {
+
+    private String userId;
+    private String userPw;
+    private String userPwRe;
+    private String userNm;
+    private String email;
+    private String mobile;
+    private boolean[] agrees;
+
+
+}
---
+++ src/main/java/org/koreait/models/member/MemberSaveService.java
@@ -0,0 +1,14 @@
+package org.koreait.models.member;
+
+import org.springframework.stereotype.Service;
+
+/*
+* 회원정보 추가 , 수정
+* - 수정시 비밀번호는 값이 있을때만 수정
+*/
+@Service
+public class MemberSaveService {
+
+
+
+}
